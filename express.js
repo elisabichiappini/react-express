f5bc9dd473c52d87af7da4b2404b1fd09439af66
elisabichiappini <elisabichiappini@gmail.com>
2025-01-22 17:42:50 +0100

    feat: add express js

--- express.js
+++ express.js
@@ -4,7 +4,16 @@ dotenv.config();
 
 //modulo express
 const express = require("express");
-const port = process.env.PORT || 8080;
-const host = process.env.HOST || "localhost";
+//per importare il mio modulo express devo creare la mia app
+const app = express();
+//porta default 3000
+const port = process.env.PORT || 3000;
 
-get 
+//definiamo le rotte
+app.get('/', (req, res) => {
+    getJoke(barza => res.send(`<h1>${barza}</h1>`));
+})
+
+app.listen(port, () => {
+    console.log(`Server avviato su questa porta http://localhost:${port}`);
+})
